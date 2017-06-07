9075877da1468191d306d5600badba242edff814
James Higgins] and Niklas Long <niklas@>
2017-06-07 10:08:13 -0700

    add editing

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,19 +6,38 @@ import { Component } from '@angular/core';
   <h1>Tap Room</h1>
   <div *ngFor="let keg of kegs">
     <h2>{{keg.name}} - {{keg.brand}}</h2>
+    <h3>Details:</h3>
     <p>{{keg.alcoholContent}}% - USD {{keg.price}} - pints: {{keg.pints}}</p>
+    <button (click)="oneLessPint(keg)">Sell a pint</button>
+    <h3>Edit:</h3>
+    <form>
+      <label>Name:</label>
+      <input [(ngModel)]="keg.name" name="name">
+      <label>Brand:</label>
+      <input [(ngModel)]="keg.brand" name="brand">
+      <label>Price:</label>
+      <input [(ngModel)]="keg.price" name="price">
+      <label>Alcohol content:</label>
+      <input [(ngModel)]="keg.alcoholContent" name="alcoholContent">
+    </form>
+    <hr>
   </div>
   `
 })
 
 export class AppComponent {
   kegs: Keg[] = [
-    new Keg('Punk IPA', 'Brewdog', 5, 7, 124),
-    new Keg('Code24', '10Barrel', 5, 5, 124),
-    new Keg('Budvar', 'Budweiser', 5, 5, 124)
+    new Keg('Punk IPA', 'Brewdog', 5, 7),
+    new Keg('Code24', '10Barrel', 5, 5),
+    new Keg('Budvar', 'Budweiser', 5, 5)
   ];
+
+  oneLessPint(keg) {
+    keg.pints -= 1
+  }
 }
 
 export class Keg {
-  constructor (public name: string, public brand: string, public price: number, public alcoholContent: number, public pints: number) { }
+  public pints: number = 124;
+  constructor (public name: string, public brand: string, public price: number, public alcoholContent: number) { }
 }
